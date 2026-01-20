17d72b42f5736599925a6cafbb782aaca7ccf4e8
whoroopamgupta <roopamgupta68@gmail.com>
2026-01-20 21:55:48 +0530

    js begon

---
+++ index.js
@@ -0,0 +1,59 @@
+// const button = document.querySelector('button');
+
+// button.addEventListener('click', () => {
+//     alert('Hello World!');
+// });
+
+// console.log("Hi, my name is roopam!")
+// console.log(25);
+
+// This is a single line comment
+
+/*
+This is a 
+multi-line comment
+*/
+
+// Variables - store, access, manipulate data
+// let, var, const
+
+// let firstName = "Roopam Gupta";
+// console.log(firstName)
+// console.log(firstName)
+// console.log(firstName)
+
+// firstName = "John";
+// console.log(firstName);
+
+
+// var - can be defined & declared again
+var firstVar = 25;
+console.log(firstVar);
+
+firstVar = 26;
+console.log(firstVar);
+
+var firstVar = 100;
+console.log(firstVar);
+
+
+// let - can be defined but not declared again
+let secVar = 30;
+console.log(secVar);
+
+secVar = 31;
+console.log(secVar);
+
+// let secVar = 300;
+// console.log(secVar);
+
+
+
+// const - constant value, cannot be changed
+const thirdVar = 35; 
+console.log(thirdVar);
+
+// thirdVar = 36;
+// console.log(thirdVar);
+
+
