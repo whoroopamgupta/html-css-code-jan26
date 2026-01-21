4730e871a26cab3955fdfd3e9d6be3e332a6995d
whoroopamgupta <roopamgupta68@gmail.com>
2026-01-21 21:44:27 +0530

    code

--- index.js
+++ index.js
@@ -26,34 +26,163 @@ multi-line comment
 // console.log(firstName);
 
 
-// var - can be defined & declared again
-var firstVar = 25;
-console.log(firstVar);
+// // var - can be defined & declared again
+// var firstVar = 25;
+// console.log(firstVar);
 
-firstVar = 26;
-console.log(firstVar);
+// firstVar = 26;
+// console.log(firstVar);
 
-var firstVar = 100;
-console.log(firstVar);
+// var firstVar = 100;
+// console.log(firstVar);
 
 
-// let - can be defined but not declared again
-let secVar = 30;
-console.log(secVar);
-
-secVar = 31;
-console.log(secVar);
-
-// let secVar = 300;
+// // let - can be defined but not declared again
+// let secVar = 30;
 // console.log(secVar);
 
+// secVar = 31;
+// console.log(secVar);
+
+// // let secVar = 300;
+// // console.log(secVar);
 
 
-// const - constant value, cannot be changed
-const thirdVar = 35; 
-console.log(thirdVar);
 
-// thirdVar = 36;
+// // const - constant value, cannot be changed
+// const thirdVar = 35; 
 // console.log(thirdVar);
+
+// // thirdVar = 36;
+// // console.log(thirdVar);
+
+
+// Data Types - primtive/non-primitive
+// Primitive Data Types
+
+// // string
+// let firstName = "John Doe";
+// console.log(firstName);
+// console.log(typeof firstName);
+
+// // number
+// let num = 25;
+// console.log(num);
+// console.log(typeof num);
+
+// // Boolean - true/false
+// let isMarried = false;
+// console.log(isMarried);
+// console.log(typeof isMarried);
+
+// // Undefined
+// let lastName;
+// console.log(typeof lastName);
+
+// // Null
+// let value = null;
+// console.log(value);
+// console.log(typeof value);
+
+// // Symbol
+
+
+// let randomValue = "random string";
+// randomValue = 45;
+// console.log(randomValue);
+
+
+// // Numbers
+// let number = 40;
+// console.log(number);
+
+// number = number + 1;
+// number+=1;
+// console.log(number);
+
+// number++;
+// number++;
+// number++;
+// number++;
+// console.log(number);
+
+
+// number+=5;
+// console.log(number);
+
+
+// number--;
+// number--;
+// console.log(number);
+
+// number-=5;
+// console.log(number);
+
+
+// number*=5;
+// console.log(number);
+
+// number/=5;
+// console.log(number);
+
+
+// String concatenation
+// const sentence = "my full name is" + " John Doe"
+// const firstName = "John";
+// const lastname = "Doe";
+// const fullName = firstName + " " + lastname;
+// console.log(fullName);
+
+// const sentence = "my full name is " + fullName;
+// console.log(sentence);
+
+
+// template literals  - ${}
+// const firstName = "John"
+// const lastname = "Doe"
+// const sentence = "Hi, my first name is " + firstName + " and my last name is " + lastname + "."
+// console.log(sentence)
+
+
+// const sentence2 = `Hi, my first name is ${firstName} and my last name is ${lastname}.`
+// console.log(sentence2)
+
+// console.log(3 + 2);
+// console.log(3 - 2);
+
+// console.log('3' + 2); // string concatenation
+// console.log('3' - 2); // maths
+// console.log('3' * 2); // maths
+// console.log('3' / 2); // maths
+
+// // ! - not
+// let isMarried = false;
+// console.log(!isMarried);
+
+// let educated = true;
+// console.log(educated);
+
+// console.log(!educated);
+
+
+// ==, ===
+// == : compare the value
+// === : compare value & type
+
+let number1 = 6;
+let number2 = '6';
+
+console.log(number1==number2);
+console.log(number1===number2);
+
+
+console.log(7=='7');
+console.log(7==='7');
+
+
+
+
+
+
 
 
